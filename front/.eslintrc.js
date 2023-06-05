be5d7ea7dd6f783b547da57acece1d0fa1f94be5
Esscraye <esscrayebat@netcourrier.com>
2023-06-05 10:58:06 +0200

    chore(package): init

---
+++ front/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    'class-methods-use-this': 0,
+    'comma-dangle': ['error', 'never'],
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows']
+  }
+};
---
+++ front/src/index.js
@@ -0,0 +1,5 @@
+import './index.scss';
+
+const hello = 'hello';
+
+console.log(hello);
