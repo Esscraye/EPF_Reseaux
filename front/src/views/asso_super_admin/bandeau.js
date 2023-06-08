08f99cc226359d4655ee8f76b22440cbe8b2109c
yann <yann.hurelle@gmail.com>
2023-06-08 12:32:58 +0200

    creation du js du site asso super admin, avec views et controller

---
+++ front/src/views/asso_super_admin/bandeau.js
@@ -0,0 +1,21 @@
+export default () => (`
+<div class="container-fluid text-center">
+<div class="row shadow-lg p-2">
+    <div class="col-2">
+      <img width="200" src="https://upload.wikimedia.org/wikipedia/fr/thumb/d/d3/Logo_FAGE.svg/langfr-1920px-Logo_FAGE.svg.png" alt="logo" />
+    </div>
+    <div class="col-8">
+      <h3 class="pt-5">My Assoc</h3>
+    </div>
+    <div class="col-2">
+        <div class="row mt-5">
+            <div class="col-4"><button type="button" class="btn btn-primary btn-sm">Supprimer</button>
+            </div>
+            <div class="col-4"><button type="button" class="btn btn-primary btn-sm">membres</button>
+            </div>
+            <div class="col-4"><button type="button" class="btn btn-primary btn-sm">Modifier</button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`);
