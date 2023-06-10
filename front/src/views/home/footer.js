eea1f8466f7cba612238854c0a613825838c057a
Esscraye <esscrayebat@netcourrier.com>
2023-06-10 18:53:10 +0200

    fix(footer): fix les div qui cassaient l'affichage du background

--- front/src/views/home/footer.js
+++ front/src/views/home/footer.js
@@ -1,16 +1,13 @@
 export default () => (`
-<h5 class="card">
-EPF Intranet
-</h5>
-<div class="card">
-<div class="card-body">
-  <p class="card-text">Email: epfreseau@epfedu.fr</p>
-  <a class="lien" target="_blank" href="https://www.instagram.com/">Instagram</a>
+<h5>EPF Intranet</h5>
+<div>
+  <p>Email: epfreseau@epfedu.fr</p>
+  <a target="_blank" href="https://www.instagram.com/">Instagram</a>
 </div>
-</div>
-<div class = "card">
-<a class="lien" target="_blank" href="mentionleg.html">Mentions légales</a>
+<div>
+<a target="_blank" href="mentionleg.html">Mentions légales</a>
 <br>
-<p class="card-text">All copyrights reserved</p>  
+<br>
+<p class="">All copyrights reserved</p>
 </div>
 `);
