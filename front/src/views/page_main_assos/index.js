ac622fe509e9f4d3d3b7cd4d53466e4c063ae3c2
root <root@DESKTOP-9S7BKU1>
2023-06-12 15:08:49 +0200

    ajout du format donnee

--- front/src/views/page_main_assos/index.js
+++ front/src/views/page_main_assos/index.js
@@ -1,86 +1,39 @@
-import header from '../home/header';
-import footer from '../home/footer';
 import card from './card';
 
-export default () => (
-  `<header>
-        ${header()}
-    </header>
-    <main>
-        <div class="container-fluid">
-            <div>
-                <h1 class="text-center pt-3 fw-bold">Portail des associations</h1>
-                <h4 class="fw-bold px-2 pb-2">Montpelier :</h4>
-                <div class="container-fluid text-center">
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                    </div>
-                </div>
-
-                <h4 class="fw-bold px-2 pb-2">Paris-Cachan :</h4>
-                <div class="container-fluid text-center">
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                    </div>
-                </div>
-
-                <h4 class="fw-bold px-2 pb-2">Saint-Nazaire :</h4>
-                <div class="container-fluid text-center">
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                    </div>
-                </div>
-
-                <h4 class="fw-bold px-2 pb-2">Troyes :</h4>
-                <div class="container-fluid text-center">
-                    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                        ${card()}
-                    </div>
-                </div>
-            </div>
+export default (data) => {
+  const { cards } = data;
+  return `
+    <div class="container-fluid">
+      <div>
+        <h1 class="text-center pt-3 fw-bold">Portail des associations</h1>
+        <h4 class="fw-bold px-2 pb-2">Montpellier :</h4>
+        <div class="container-fluid text-center">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
+          ${cards.map(card).join('')}
+          </div>
         </div>
-    </main>
-    <footer>
-        ${footer()}
-    </footer>`
-);
+
+        <h4 class="fw-bold px-2 pb-2">Paris-Cachan :</h4>
+        <div class="container-fluid text-center">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
+          ${cards.map(card).join('')}
+          </div>
+        </div>
+
+        <h4 class="fw-bold px-2 pb-2">Saint-Nazaire :</h4>
+        <div class="container-fluid text-center">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
+          ${cards.map(card).join('')}
+          </div>
+        </div>
+
+        <h4 class="fw-bold px-2 pb-2">Troyes :</h4>
+        <div class="container-fluid text-center">
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4">
+          ${cards.map(card).join('')}
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+};
