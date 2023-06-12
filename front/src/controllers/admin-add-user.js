68853aad7521a7e2640032fdfd31f5911636e6f0
Amelie <ameliie.rikaud@gmail.com>
2023-06-12 14:04:17 +0200

    Mise à jour pour utiliser les fichiers page

---
+++ front/src/controllers/admin-add-user.js
@@ -0,0 +1,34 @@
+import { isEmail } from 'validator';
+import ControllerPage from './page';
+import ViewAdminAddUser from '../views/admin-add-user';
+
+const AdminAddUser = class AdminAddUser {
+  constructor() {
+    this.el = document.body;
+
+    this.run();
+  }
+
+  onClickAjouter() {
+    const elInput = document.querySelector('email');
+    const elBtn = document.querySelector('ajouter');
+
+    elBtn.addEventListener('click', () => {
+      if (elInput.value && isEmail(elInput.value)) {
+        console.log({
+          email: elInput.value
+        });
+        elInput.value = '';
+      } else {
+        alert('Problem');
+      }
+    });
+  }
+
+  run() {
+    new ControllerPage(ViewAdminAddUser());
+    this.onClickAjouter();
+  }
+};
+
+export default AdminAddUser;
