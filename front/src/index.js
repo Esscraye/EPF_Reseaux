d0acd4318b85b5cdeb11eb5ae830ae78274f9d4a
root <root@PC-Marine>
2023-06-12 16:57:59 +0200

    ajout des rooter pour Toutes les pages

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,21 @@
 import Router from 'vanilla-router';
 
-import ControllerHome from './controllers/home';
+import Home from './controllers/home';
 import Page404 from './controllers/page-404';
+import AddUser from './controllers/admin-ajouter-user';
+import ChangeUser from './controllers/admin-modifier-user';
+import DispProf from './controllers/afficher-profil';
+import AddGrp from './controllers/ajouter-groupe';
+import AssosFondResp from './controllers/asso-fondateur-et-responsable';
+import AssosSuperAdmin from './controllers/asso-super-admin';
+import AddAssosUser from './controllers/asso-utilisateur';
+import Connect from './controllers/connection';
+import CreateArticle from './controllers/creaarticle';
+import CreateAssos from './controllers/createAssos';
+import Discu from './controllers/discu';
+import GestMember from './controllers/gestionmembre';
+import MDPForgot from './controllers/mdp_oubli';
+import HomeAssos from './controllers/page_main_assos';
 
 import './index.scss';
 
@@ -11,10 +25,50 @@ const router = new Router({
     new Page404();
   }
 });
-
 router.add('', () => {
-  new ControllerHome();
+  new Home();
 });
-
+router.add('/addUser', () => {
+  new AddUser();
+});
+router.add('/changeUser', () => {
+  new ChangeUser();
+});
+router.add('/profil', () => {
+  new DispProf();
+});
+router.add('/addGrp', () => {
+  new AddGrp();
+});
+router.add('/assosFond&Resp', () => {
+  new AssosFondResp();
+});
+router.add('/assosSuperAdmin', () => {
+  new AssosSuperAdmin();
+});
+router.add('/addAssosUser', () => {
+  new AddAssosUser();
+});
+router.add('/connect', () => {
+  new Connect();
+});
+router.add('/createArticle', () => {
+  new CreateArticle();
+});
+router.add('/createAssos', () => {
+  new CreateAssos();
+});
+router.add('/discu', () => {
+  new Discu();
+});
+router.add('/gestMember', () => {
+  new GestMember();
+});
+router.add('/mdpForgot', () => {
+  new MDPForgot();
+});
+router.add('/homeAssos', () => {
+  new HomeAssos();
+});
 const { pathname } = location;
 router.navigateTo(pathname);
