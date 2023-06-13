98d1c8468a3ab97db0e7c7325b71fd3724d50118
Alban <alban.lauby@gmail.com>
2023-06-13 08:04:38 +0200

    modification de la nav-bar (pas encore finis)

---
+++ front/src/views/home/footer.js
@@ -0,0 +1,13 @@
+export default () => (`
+<h5>EPF Intranet</h5>
+<div>
+  <p>Email: epfreseau@epfedu.fr</p>
+  <a target="_blank" href="https://www.instagram.com/">Instagram</a>
+</div>
+<div>
+<a target="_blank" href="mentionleg.html">Mentions légales</a>
+<br>
+<br>
+<p class="">All copyrights reserved</p>
+</div>
+`);
