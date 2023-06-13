96d93811416719b424f5b101fbe8ab1df3541155
Alban <alban.lauby@gmail.com>
2023-06-13 18:38:24 +0200

    update footer

--- front/src/views/page/footer.js
+++ front/src/views/page/footer.js
@@ -1,11 +1,32 @@
 export default () => (`
-  <div class="navbar-brand log">EPF Intranet</div>  
-  <div>
-    <p>Email: epfreseau@epfedu.fr</p>
-    <a target="_blank" href="https://www.instagram.com/" class="btn btn-nav">Instagram</a>
+  <div class="cont-foot">
+    <h5 class="navbar-brand log">EPF Intranet</h5>  
   </div>
-  <div>
-    <a target="_blank" href="mentionleg.html" class="btn btn-nav">Mentions légales</a>
-    <p class="">All copyrights reserved</p>
+  <div class="cont-foot">
+    <h4>Useful link</h4>  
+    <a href="https://moodle.epf.fr/login/epf/login.html" class="ender-barre">Moodle</a>
+    <a href="https://my.epf.fr/" class="ender-barre">Myepf</a>
+    <a href="https://epfedu.sharepoint.com/sites/INSERTIONPROFESSIONNELLE2" class="ender-barre">Stage</a>
+    <div class="reseaux">
+      <a href="#" target="_blank" class=""><div class="content-reseaux"><i class="ri-instagram-fill"></i></div></a>
+      <a href="#" target="_blank" class=""><div class="content-reseaux"><i class="ri-twitter-fill"></i></div></a>
+      <a href="#" target="_blank" class=""><div class="content-reseaux"><i class="ri-facebook-box-fill"></i></div></a>
+      <a href="#" target="_blank" class=""><div class="content-reseaux"><i class="ri-linkedin-box-fill"></i></div></a>
+    </div>
+  </div>
+  <div class="cont-foot">
+    <h4>Quick Links</h4>
+    <a href="#" class="ender-barre">Accueil</a>
+    <a href="/homeAssos" class="ender-barre">Association</a>
+    <a href="/profil" class="ender-barre">Profil</a>
+    <a href="#" class="ender-barre">Admin</a>
+  </div>
+  <div class="cont-foot">
+    <h4 class="">Contact us</h4>
+    <a href="mentionleg.html" class="ender-barre">Mentions légales</a>
   </div>
 `);
+
+// Email: epfreseau@epfedu.fr
+// All copyrights reserved
+// moodle - myepf - stae
