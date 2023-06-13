20e933b04cda32c545b859681c9ff484a0ec85d3
root <root@PC-Marine>
2023-06-13 09:00:46 +0200

    changement d'un nom de fichier, ajout des Controllers dans index main, suppression des erreurs du fichier de julie

--- front/src/index.js
+++ front/src/index.js
@@ -1,74 +1,74 @@
 import Router from 'vanilla-router';
 
-import Home from './controllers/home';
-import Page404 from './controllers/page-404';
-import AddUser from './controllers/admin-ajouter-user';
-import ChangeUser from './controllers/admin-modifier-user';
-import DispProf from './controllers/afficher-profil';
-import AddGrp from './controllers/ajouter-groupe';
-import AssosFondResp from './controllers/asso-fondateur-et-responsable';
-import AssosSuperAdmin from './controllers/asso-super-admin';
-import AddAssosUser from './controllers/asso-utilisateur';
-import Connect from './controllers/connection';
-import CreateArticle from './controllers/creaarticle';
-import CreateAssos from './controllers/createAssos';
-import Discu from './controllers/discu';
-import GestMember from './controllers/gestionmembre';
-import MDPForgot from './controllers/mdp_oubli';
-import HomeAssos from './controllers/page_main_assos';
+import ControllerHome from './controllers/home';
+import ControllerPage404 from './controllers/page-404';
+import ControllerAddUser from './controllers/admin-add-user';
+import ControllerChangeUser from './controllers/admin-modify-user';
+import ControllerDispProf from './controllers/afficher-profil';
+import ControllerAddGrp from './controllers/ajouter-groupe';
+import ControllerAssosFondResp from './controllers/asso-fondateur-et-responsable';
+import ControllerAssosSuperAdmin from './controllers/asso-super-admin';
+import ControllerAddAssosUser from './controllers/asso-utilisateur';
+import ControllerConnect from './controllers/connection';
+import ControllerCreateArticle from './controllers/creaarticle';
+import ControllerCreateAssos from './controllers/create-assos';
+import ControllerDiscu from './controllers/discu';
+import ControllerGestMember from './controllers/gestionmembre';
+import ControllerMDPForgot from './controllers/mdp_oubli';
+import ControllerHomeAssos from './controllers/page_main_assos';
 
 import './index.scss';
 
 const router = new Router({
   mode: 'history',
   page404: () => {
-    new Page404();
+    new ControllerPage404();
   }
 });
 router.add('', () => {
-  new Home();
+  new ControllerHome();
 });
 router.add('/addUser', () => {
-  new AddUser();
+  new ControllerAddUser();
 });
 router.add('/changeUser', () => {
-  new ChangeUser();
+  new ControllerChangeUser();
 });
 router.add('/profil', () => {
-  new DispProf();
+  new ControllerDispProf();
 });
 router.add('/addGrp', () => {
-  new AddGrp();
+  new ControllerAddGrp();
 });
 router.add('/assosFond&Resp', () => {
-  new AssosFondResp();
+  new ControllerAssosFondResp();
 });
 router.add('/assosSuperAdmin', () => {
-  new AssosSuperAdmin();
+  new ControllerAssosSuperAdmin();
 });
 router.add('/addAssosUser', () => {
-  new AddAssosUser();
+  new ControllerAddAssosUser();
 });
 router.add('/connect', () => {
-  new Connect();
+  new ControllerConnect();
 });
 router.add('/createArticle', () => {
-  new CreateArticle();
+  new ControllerCreateArticle();
 });
 router.add('/createAssos', () => {
-  new CreateAssos();
+  new ControllerCreateAssos();
 });
 router.add('/discu', () => {
-  new Discu();
+  new ControllerDiscu();
 });
 router.add('/gestMember', () => {
-  new GestMember();
+  new ControllerGestMember();
 });
 router.add('/mdpForgot', () => {
-  new MDPForgot();
+  new ControllerMDPForgot();
 });
 router.add('/homeAssos', () => {
-  new HomeAssos();
+  new ControllerHomeAssos();
 });
 const { pathname } = location;
 router.navigateTo(pathname);
