ac31d1a5af4178371abdd2ad1fbb403021cd404a
Alban <alban.lauby@gmail.com>
2023-06-13 14:33:23 +0200

    début modif footer

--- front/src/views/page/footer.js
+++ front/src/views/page/footer.js
@@ -1,13 +1,11 @@
 export default () => (`
-  <h5>EPF Intranet</h5>
+  <div class="navbar-brand log">EPF Intranet</div>  
   <div>
     <p>Email: epfreseau@epfedu.fr</p>
-    <a target="_blank" href="https://www.instagram.com/">Instagram</a>
+    <a target="_blank" href="https://www.instagram.com/" class="btn btn-nav">Instagram</a>
   </div>
   <div>
-    <a target="_blank" href="mentionleg.html">Mentions légales</a>
-    <br>
-    <br>
+    <a target="_blank" href="mentionleg.html" class="btn btn-nav">Mentions légales</a>
     <p class="">All copyrights reserved</p>
   </div>
 `);
