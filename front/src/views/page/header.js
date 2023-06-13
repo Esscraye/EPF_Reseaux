7a459593a7ba0786ba1090869da9e4546e9c2fe8
Alban <alban.lauby@gmail.com>
2023-06-13 09:38:36 +0200

    fin des modif de la barre de navigation

--- front/src/views/page/header.js
+++ front/src/views/page/header.js
@@ -1,11 +1,13 @@
 export default () => (`
 <nav class="">
-  <div class="navbar-brand log" href="#">EPF Intranet</div>
-  <a href="">Accueil</a>
-  <a href="/homeAssos">Association</a>
-  <a href="/profil">Profil</a>
-  <a href="#">Admin</a>
-  <form class="d-flex nav-search rech-part" role="search">
+  <div class="partie-g">
+    <div class="navbar-brand log" href="#">EPF Intranet</div>
+    <a href="#" class="btn btn-nav slected">Accueil</a>
+    <a href="/homeAssos" class="btn btn-nav">Association</a>
+    <a href="/profil" class="btn btn-nav">Profil</a>
+    <a href="#" class="btn btn-nav">Admin</a>
+  </div>
+  <form class="d-flex nav-search partie-d" role="search">
       <input class="form-control me-2" style="background-color: white;" type="search" placeholder="Recherche" aria-label="Search">
       <button class="btn btn-outline-success" type="button">Rechercher</button>
     </form>
