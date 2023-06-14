ffc77d0eeae0557b7721668c20bbceb0054e308d
root <root@DESKTOP-89NEU20.localdomain>
2023-06-14 15:41:14 +0200

    modification des pages serveurs et route pour rajouter group

--- api/src/server.mjs
+++ api/src/server.mjs
@@ -71,6 +71,7 @@ const Server = class Server {
   routes() {
     new routes.Users(this.app, this.connect);
     new routes.Assoc(this.app, this.connect);
+    new routes.Groups(this.app, this.connect);
 
     this.app.use((req, res) => {
       res.status(404).json({
