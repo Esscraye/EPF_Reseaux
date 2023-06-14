c4665cf23dad94ea4bd896fe54e979b0599d6592
maxence <deathloadtv@gmail.com>
2023-06-14 14:41:59 +0200

    les data ont été réduit pour n'en transmettre qu'une

--- front/src/views/discussion/barre_discu_solo.js
+++ front/src/views/discussion/barre_discu_solo.js
@@ -1,9 +1,9 @@
-export default (data) => (`
+export default (name, icone) => (`
   <div class="row bg-danger border border-dark border-end-0 border-start-0 border-top-0">
     <div class="col-3 mt-1">
       <div class="row text-center mx-0">
-        <div class="col-3 mb-1"><img src="${data.icone}" class="rounded-circle" alt="..." width="40"></div>
-        <div class="d-flex justify-content-between align-items-center bg-danger text-white col-9">${data.name}</div>
+        <div class="col-3 mb-1"><img src="${icone}" class="rounded-circle" alt="..." width="40"></div>
+        <div class="d-flex justify-content-between align-items-center bg-danger text-white col-9">${name}</div>
       </div>
     </div>
     <div class="col-7 my-1 text-center"></div>
--- front/src/views/discussion/index.js
+++ front/src/views/discussion/index.js
@@ -1,6 +1,6 @@
 import selection from './selction';
-import barreDiscuSolo from './barre_discu_solo';
 import barreDiscuGroup from './barre_discu_groupe';
+import barreDiscuPerso from './barre_discu_solo';
 import barreEnvoiMess from './barre_envoie_mess';
 import messG from './message_gauche';
 import messD from './message_droite';
@@ -8,27 +8,36 @@ import messD from './message_droite';
 import './index.scss';
 
 export default (data) => {
-  const {
-    choicesG, choicesP, soloBar, groupBar, conversations
-  } = data;
+  const { conversations } = data;
 
-  const conversationId = '21';
-  const conversation = conversations.find((conv) => conv.convId === conversationId);
+  const conversation = conversations.find((conv) => conv.convId === '12');
 
-  return (`
-    <div class="row">
-      <div class="col-3 liste-conv bg-danger">
-        ${selection(choicesG, choicesP)}  
-      </div>
-      <div class="col-9 messages-container">
-        ${conversationId === '12' ? barreDiscuGroup(groupBar) : barreDiscuSolo(soloBar)}
-        <div class="messages">
-        ${conversation.messages
-      .map((message) => (message.author.email === 'maxence.juery@epfedu.fr' ? messD(message) : messG(message)))
-      .join('')}
+  if (conversation) {
+    const messagesHTML = conversation.messages.map((message) => (message.author.email === 'maxence.juery@epfedu.fr' ? messD(message) : messG(message))).join('');
+
+    let barreDiscuComponent;
+    if (conversation.type === 'group') {
+      barreDiscuComponent = barreDiscuGroup(conversation.name, conversation.icone);
+    } else if (conversation.type === 'individual') {
+      barreDiscuComponent = barreDiscuPerso(conversation.name, conversation.icone);
+    }
+
+    return (`
+      <div class="row">
+        <div class="col-3 liste-conv bg-danger">
+          ${selection(conversations)}  
         </div>
-        ${barreEnvoiMess()}
+        <div class="col-9 messages-container">
+          ${barreDiscuComponent}
+          <div class="messages">
+            ${messagesHTML}
+          </div>
+          ${barreEnvoiMess()}
+        </div>
       </div>
-    </div>
-  `);
+    `);
+  }
+
+  // Gérer le cas où aucune conversation de type "group" ou "individual" n'est trouvée
+  return "Aucune conversation de type 'group' ou 'individual' n'est disponible.";
 };
