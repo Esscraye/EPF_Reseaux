95d0b1f029c9d8d4990815aff41c9395ee1a9170
Baleras Léo <leo.baleras@epfedu.fr>
2023-06-14 12:15:10 +0200

    Update config.mjs change the mongodb connection

--- api/src/config.mjs
+++ api/src/config.mjs
@@ -2,11 +2,11 @@ export default {
   development: {
     type: 'development',
     port: 3000,
-    mongodb: 'mongodb+srv://yhurelle:Atlas44@epf.httwisv.mongodb.net/EPF'
+    mongodb: 'mongodb+srv://12345:12345@demo.v4muu5b.mongodb.net/ecole'
   },
   production: {
     type: 'production',
     port: 3000,
-    mongodb: 'mongodb+srv://yhurelle:Atlas44@epf.httwisv.mongodb.net/EPF'
+    mongodb: 'mongodb+srv://12345:12345@demo.v4muu5b.mongodb.net/ecole'
   }
 };
