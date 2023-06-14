2bebd0a40f012c3df2a461232819fc46aa2d5699
Alban <alban.lauby@gmail.com>
2023-06-14 16:13:59 +0200

    modif frontend profil

--- front/src/views/show-profile/index.js
+++ front/src/views/show-profile/index.js
@@ -6,52 +6,46 @@ export default (data) => {
     <div class="container.fluid">
       <div class="row mb-5" >
         <div class="col-3 container-info-prof">
-          <img src="${infoPerso.picture}" alt="Image utilisateur" class="imageprof image-prof" id ="affprofimage">
-          <div class = "name" id = "affprofprenomnom">
-            <strong>
+          <img src="${infoPerso.picture}" alt="Image utilisateur" class=" rounded-circle imageprof image-prof" id ="affprofimage">
+          <div class ="text-gras">
               ${infoPerso.firstName} ${infoPerso.lastName}
-            </strong>
           </div>
-          <div class="modifprof" id="affprofmodifprof">
+          <div class="">
             <a href="/changeUser" class="btn btn-danger mr-2" role="button" id ="btnmodifprofil">Modifier profil</a>
           </div>
         </div>
         <div class="col-6">
           <div class ="border shadow-manuelle container-profil" id="affprofborder">
-            <div class = "infoperso" id ="affprofinfo">
-              <strong>
+            <div class ="text-gras">
                 Informations personnelles
-              </strong>
             </div>
-            <div class = "prenom" id ="affprofprenom">
+            <div class ="info-profil">
               Prénom : ${infoPerso.firstName}
             </div>
-            <div class = "nom" id ="affprofnom">
+            <div class ="info-profil">
               Nom : ${infoPerso.lastName}
             </div>
-            <div class = "email" id ="affprofmail">
+            <div class ="info-profil">
               Email : ${infoPerso.email}
             </div>
-            <div class = "description" id ="affprofdesc">
+            <div class ="info-profil">
             Description : ${infoPerso.description}
             </div>
           </div>
-          <div class = "border shadow-manuelle container-profil" id="affprofborder">
-            <div class = "grou" id = "affprofgrou">
-              <strong>
-                Groupes
-              </strong>
+          <div class ="border shadow-manuelle container-profil">
+            <div class = "text-gras">
+              Groupes
             </div>
-            <div class = "promo" id ="affprofpromo">
+            <div class ="info-profil">
               Promo : ${infoPerso.promo}
             </div>
-            <div class = "classe" id= "affprofclasse">
+            <div class ="info-profil">
               Classe : ${infoPerso.class}
             </div>
-            <div class = "groupedetp" id="affprofgroutp">
+            <div class ="info-profil">
               Groupe de TP : ${infoPerso.groupTp}
             </div>
-            <div class = "association" id ="affprofasso">
+            <div class ="info-profil">
               Associations : ${infoPerso.associations}
             </div>
           </div>
