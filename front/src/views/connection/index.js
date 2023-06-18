37baacfbd2d6b47ba3a36509c44f8e41a0698227
Louis <louis.rideau.02@gmail.com>
2023-06-18 18:03:16 +0200

    correction de l'html et css de la page connection

--- front/src/views/connection/index.js
+++ front/src/views/connection/index.js
@@ -1,30 +1,28 @@
 import './index.scss';
 
 export default () => (`
-  <div class="container connection">
-    <div class="row">
-      <div class="col-6 part-g">
-        <div class="container-photo">
-          <img src="https://www.univ-lyon2.fr/medias/photo/illus-pole-asso_1594492888834-png?ID_FICHE=1006216" alt="Photo epf" width="70%" height="95%">
-        </div>
-      </div>
-      <div class="col-4 mt-5 text-center">
-        <div class="border rounded">
+<main class="grid-connect">
+<section class="sect-1">
+          <img class="image-connect" src="https://www.univ-lyon2.fr/medias/photo/illus-pole-asso_1594492888834-png?ID_FICHE=1006216" alt="Photo epf">
+</section>
+<section class="sect-2">
+        <div>
           <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/e/e9/EPF_logo_2021.png/640px-EPF_logo_2021.png" alt="logo epf" width="100" height="80">
-          <div align="center">
-            <div class="mb-3">
+        <div align="center">
+            <div class="mb-3 mt-1">
               <label for="exampleFormControlInput1" class="form-label">E-mail</label>
-              <input type="email" class="form-control exampleFormControlInput1" placeholder="email">
+              <input type="email" class="form-control exampleFormControlInput1" placeholder="Votre email">
             </div>
             <div class="mb-3">
               <label for="exampleFormControlInput2" class="form-label">Mot de passe</label>
-              <input type="password" class="form-control exampleFormControlInput2" placeholder="mdp">
+              <input type="password" class="form-control exampleFormControlInput2" placeholder="Votre mot de passe">
             </div>
           </div>
           <button type="button" class="btn bt">Connexion</button> 
-          <p><a href="/password-forgot" class="link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Mot de passe oublié ?</a></p>
+          <p><a href="/password-forgot" class="underline-connect link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Mot de passe oublié ?</a></p>
         </div>
-      </div>
     </div>
     </div>
+</section>
+</main>
 `);
