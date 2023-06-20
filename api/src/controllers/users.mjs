9ab56ca1c2b59d1e2d4c074fe1ee720d0b677c29
Esscraye <esscrayebat@netcourrier.com>
2023-06-20 21:23:06 +0200

    add and edit all user creation, user login, user editing

--- api/src/controllers/users.mjs
+++ api/src/controllers/users.mjs
@@ -1,5 +1,9 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import UserModel from '../models/user.mjs';
 import authToken from '../hook/auth.mjs';
+
+dotenv.config();
 
 const Users = class Users {
   constructor(app, connect) {
@@ -10,7 +14,7 @@ const Users = class Users {
   }
 
   deleteById() {
-    this.app.delete('/user/:id', (req, res) => {
+    this.app.delete('/user/:id', authToken, (req, res) => {
       try {
         this.UserModel.findByIdAndDelete(req.params.id).then((user) => {
           res.status(200).json(user || {});
@@ -54,7 +58,7 @@ const Users = class Users {
   }
 
   create() {
-    this.app.post('/user/', (req, res) => {
+    this.app.post('/user/', authToken, (req, res) => {
       try {
         const userModel = new this.UserModel(req.body);
 
@@ -74,11 +78,62 @@ const Users = class Users {
     });
   }
 
-  update() {
-    this.app.put('/users/:id', authToken, (req, res) => {
+  login() {
+    this.app.post('/login/', (req, res) => {
       try {
-        this.UsersModel.findByIdAndUpdate(req.params.id, req.body).then(() => {
-          res.status(200).json("ok c'est good" || {});
+        const { email, password } = req.body;
+
+        this.UserModel.findOne({ email }).then((user) => {
+          if (!user) {
+            res.status(404).json({
+              code: 404,
+              message: 'User not found'
+            });
+          } else if (user.password === password) {
+            const { role, assos } = user;
+            const token = jwt.sign({ email, role, assos }, process.env.JWT_TOKEN, { expiresIn: '24h' });
+            res.status(200).json({
+              token
+            });
+          } else {
+            res.status(401).json({
+              code: 401,
+              message: 'Unauthorized'
+            });
+          }
+        }).catch((err) => {
+          res.status(500).json({
+            code: 500,
+            error: err,
+            message: 'Internal Server error'
+          });
+        });
+      } catch (err) {
+        console.error(`[ERROR] users/login -> ${err}`);
+
+        res.status(400).json({
+          code: 400,
+          message: 'Bad request'
+        });
+      }
+    });
+  }
+
+  updateProfile() {
+    this.app.put('/user/:id', authToken, (req, res) => {
+      try {
+        const { id } = req.params;
+        const {
+          firstname, lastname, email, description
+        } = req.body;
+
+        this.UserModel.findByIdAndUpdate(id, {
+          firstname,
+          lastname,
+          email,
+          description
+        }).then((user) => {
+          res.status(200).json(user || {});
         }).catch(() => {
           res.status(500).json({
             code: 500,
@@ -100,7 +155,8 @@ const Users = class Users {
     this.create();
     this.showByEmail();
     this.deleteById();
-    this.update();
+    this.login();
+    this.updateProfile();
   }
 };
 
