03f6562020d02c683c21c53ed117f6749f1a9a30
root <root@DESKTOP-89NEU20.localdomain>
2023-06-21 15:46:17 +0200

    recherche de profil dans la navbarre fonctionne

--- front/src/controllers/show-profile.js
+++ front/src/controllers/show-profile.js
@@ -1,24 +1,47 @@
 import axios from 'axios';
+import cookie from 'js-cookie';
 import config from '../../config';
 import ViewShowProfile from '../views/show-profile';
 import ControllerPage from './page';
 
 const ShowProfile = class ShowProfile {
+  getemailuser() {
+    const token = cookie.get('token');
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map((c) => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`).join(''));
+    return JSON.parse(jsonPayload).email;
+  }
+
   constructor() {
     this.el = document.body;
     this.data = {};
-    axios.get(`${config.IP_API}/user/augustin.nadiedjoa@epfedu.fr`)
-      .then((response) => {
-        console.log(response);
-        this.data.infoPerso = response.data;
-        this.run();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+
+    this.run();
   }
 
-  run() {
+  async fetchProfilData() {
+    const queryString = window.location.search;
+    const url = new URLSearchParams(queryString);
+    const mailurl = url.get('email');
+    if (mailurl) {
+      try {
+        const response = await axios.get(`${config.IP_API}/user/${mailurl}`);
+        this.data.infoPerso = response.data;
+        // new ControllerPage(ViewShowProfile(this.data));
+      } catch (error) {
+        console.log('perdu');
+        // Gérer l'erreur
+      }
+    } else {
+      const mail = this.getemailuser();
+      const response = await axios.get(`${config.IP_API}/user/${encodeURIComponent(mail)}`);
+      this.data.infoPerso = response.data;
+    }
+  }
+
+  async run() {
+    await this.fetchProfilData();
     new ControllerPage(ViewShowProfile(this.data));
   }
 };
