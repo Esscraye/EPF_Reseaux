fc3ebd9c34653e1f5484786eeae3fbcaceba7886
justinesmmt <justine.sammut@epfedu.fr>
2023-06-22 15:01:44 +0200

    mot de passe temporaire maintenant random

--- front/src/controllers/password-forgotten.js
+++ front/src/controllers/password-forgotten.js
@@ -1,6 +1,18 @@
 import { isEmail } from 'validator';
 import ViewPasswordForgot from '../views/password-forgotten';
 
+function generateRandomPassword(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let password = '';
+
+  for (let i = 0; i < length; i += 1) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+
+  return password;
+}
+
 const PasswordForgot = class PasswordForgot {
   constructor(content) {
     this.el = document.body;
@@ -12,13 +24,24 @@ const PasswordForgot = class PasswordForgot {
     const elInput = document.querySelector('#exampleFormControlInput');
     const elBtn = document.querySelector('#btnmdp');
 
-    elBtn.addEventListener('click', (e) => {
-      e.preventDefault();
+    elBtn.addEventListener('click', () => {
       if (elInput.value && isEmail(elInput.value)) {
-        console.log({
-          email: elInput.value
-        });
+        const email = elInput.value;
+
+        // Génération d'un mot de passe temporaire
+        const temporaryPassword = generateRandomPassword(8);
+
+        // Construction du contenu de l'email
+        const emailSubject = 'Mot de passe temporaire';
+        const emailBody = `Votre mot de passe temporaire est : ${temporaryPassword}`;
+        const emailContent = `mailto:${email}?subject=${encodeURIComponent(emailSubject)}&body=${encodeURIComponent(emailBody)}`;
+
+        // Ouvre le client de messagerie par défaut avec l'email pré-rempli
+        window.location.href = emailContent;
+
         elInput.value = '';
+      } else {
+        console.log('Veuillez fournir une adresse email valide.');
       }
     });
   }
