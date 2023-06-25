05891405cb6d5f0ac74fc3ad085504c26a658b74
Esscraye <esscrayebat@netcourrier.com>
2023-06-25 22:46:51 +0200

    change connection using <form>

--- front/src/views/connection/index.js
+++ front/src/views/connection/index.js
@@ -4,7 +4,7 @@ export default () => (`
     <div>
       <img src="/images/logo.png" alt="logo epf" width="100" height="100">
     </div>
-    <div align="center">
+    <form align="center formconnection">
       <div class="mb-3 mt-1">
         <label for="exampleFormControlInput1" class="form-label">E-mail</label>
         <input type="email" class="form-control emailInput" placeholder="Votre email">
@@ -13,10 +13,10 @@ export default () => (`
         <label for="exampleFormControlInput2" class="form-label">Mot de passe</label>
         <input type="password" class="form-control passwordInput" placeholder="Votre mot de passe">
       </div>
-    </div>
-    <div>
-      <button type="button" class="btn bt connection">Connexion</button> 
-    </div>
+      <div>
+        <button type="submit" class="btn bt connection">Connexion</button> 
+      </div>
+    </form>
     <a href="/password-forgot" class="underline-connect link-dark link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Mot de passe oublié ?</a>
   </div>
 </main>
